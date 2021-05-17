a5ae0639d9410521e02e4fed24315015a40de6e2
CristianRodriguezANubox <cristian.rodrigueza@nubox.com>
2021-05-17 14:43:18 -0400

    finish

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,25 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="crisroddev/udacity"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservices\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=809
+    --labels app=microservice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservices 8000:80
+
+# Step 5
+# Get LOGS
+kubectl logs `kubectl get pods -o=name`
 
